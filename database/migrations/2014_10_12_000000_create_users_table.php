2b536cbd21db5dc6a7b35b74534068914d3ed917
fatima.mhmzd <fatima.m7821@gmail.com>
2023-05-20 20:59:11 +0330

    user migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,6 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
+
+
         Schema::create('users', function (Blueprint $table) {
             $table->comment('جدول اطلاعات اشخاص(کاربران)');
             $table->id();
